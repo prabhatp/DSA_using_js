58474f511ce05be50574d8a417611bf156432540
prabhatp <prabhakarprabhat523@gmail.com>
2023-05-01 07:36:54 +0530

    updated the code

--- linked_list_with_tail.js
+++ linked_list_with_tail.js
@@ -102,19 +102,22 @@ class LinkedList {
     }
 }
 
-const list = new LinkedList();
-console.log("List is empty", list.isEmpty());
-console.log("List size", list.getSize());
-list.print();
+// To implement the stack and queue.
+module.exports = LinkedList;
 
-list.append(1);
-list.append(2);
-list.append(3);
-list.append(0);
-list.print();
-console.log('List size', list.getSize());
+// const list = new LinkedList();
+// console.log("List is empty", list.isEmpty());
+// console.log("List size", list.getSize());
+// list.print();
 
-list.removeFromFront();
-list.removeFromEnd();
-list.print();
+// list.append(1);
+// list.append(2);
+// list.append(3);
+// list.append(0);
+// list.print();
+// console.log('List size', list.getSize());
+
+// list.removeFromFront();
+// list.removeFromEnd();
+// list.print();
 
